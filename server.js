f6c2414deee7ab4157299442a724593965fc8458
kpworthi <kpworthi@gmail.com>
2020-09-28 16:35:40 +0000

    submission state

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var express = require('express');
 var cors = require('cors');
 
 // require and use "multer"...
+var multer  = require('multer')
+var upload = multer({ dest: 'uploads/' })
 
 var app = express();
 
@@ -17,6 +19,18 @@ app.get('/', function (req, res) {
 app.get('/hello', function(req, res){
   res.json({greetings: "Hello, API"});
 });
+
+app.post('/api/fileanalyse', upload.single('upfile'), fileUpload)
+
+function fileUpload (req, res, next) {
+  let fileSize = req.file.size;
+  let fileName = req.file.originalname;
+
+  res.json({
+    "filename": fileName,
+    "filesize": fileSize
+  })
+}
 
 app.listen(process.env.PORT || 3000, function () {
   console.log('Node.js listening ...');
